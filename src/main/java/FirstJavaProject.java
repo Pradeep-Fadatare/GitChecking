3501b35cec39f9c098ba9c07f4e08cc4dbfe66f5
Pradeep Fadatare <padyafadatare@gmail.com>
2022-09-28 22:29:55 +0530

    this is commit from india

--- src/main/java/FirstJavaProject.java
+++ src/main/java/FirstJavaProject.java
@@ -1,5 +1,8 @@
 public class FirstJavaProject {
     public static void main(String[] args) {
         System.out.println("hiiii");
+        System.out.println("byy");
+        System.out.println("tata");
+        //jbsjabhh
     }
 }
